3279fb76e55f0eb5c7ab5a22486c2e4bd944d6f6
Palkaran Kaur <57713531+epalkaran@users.noreply.github.com>
2023-10-06 05:00:59 +0530

    Update README.js

--- README.js
+++ README.js
@@ -1,34 +1,19 @@
-import React from "react";
-import MultiSelectCheckbox from "./MultiSelectCheckbox";
-
-const options = ["Option 1", "Option 2", "Option 3", "Option 4"];
-
-const App = () => {
-  return (
-    <div>
-      <h1>Multi-Select Checkbox Example</h1>
-      <MultiSelectCheckbox options={options} />
-    </div>
-  );
-};
-
-export default App;
-
-
-
 import React, { useState } from "react";
-import { FormControl, FormGroup, FormControlLabel, Checkbox, Button, Typography } from "@mui/material";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const MultiSelectCheckbox = ({ options }) => {
   const [selectedOptions, setSelectedOptions] = useState([]);
-  
+
   const handleCheckboxChange = (option) => {
     const updatedOptions = selectedOptions.includes(option)
       ? selectedOptions.filter((item) => item !== option)
       : [...selectedOptions, option];
     setSelectedOptions(updatedOptions);
   };
-  
+
   const handleSubmit = () => {
     // Do something with selectedOptions, for example, store them in a state or send them to an API.
     console.log("Selected Options:", selectedOptions);
@@ -36,22 +21,18 @@ const MultiSelectCheckbox = ({ options }) => {
 
   return (
     <div>
-      <FormControl component="fieldset">
-        <FormGroup>
-          {options.map((option, index) => (
-            <FormControlLabel
-              key={index}
-              control={
-                <Checkbox
-                  checked={selectedOptions.includes(option)}
-                  onChange={() => handleCheckboxChange(option)}
-                />
-              }
-              label={option}
+      {options.map((option, index) => (
+        <FormControlLabel
+          key={index}
+          control={
+            <Checkbox
+              checked={selectedOptions.includes(option)}
+              onChange={() => handleCheckboxChange(option)}
             />
-          ))}
-        </FormGroup>
-      </FormControl>
+          }
+          label={option}
+        />
+      ))}
       <div style={{ marginTop: 20 }}>
         <Typography variant="h6">Selected Options:</Typography>
         {selectedOptions.map((option, index) => (
@@ -66,4 +47,3 @@ const MultiSelectCheckbox = ({ options }) => {
 };
 
 export default MultiSelectCheckbox;
-
